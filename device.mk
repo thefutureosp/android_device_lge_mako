e6ab75a5945a6a0f2430eb77c0fb5479cbe40fb9
wjb <jaybob413@gmail.com>
2012-11-28 18:16:04 -0500

    Use prebuilt kernel for now
    Change-Id: Ic884d2e64460fc7381035ea97df5a9b6336ae1e4

--- device.mk
+++ device.mk
@@ -42,7 +42,14 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/lge/mako/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
 PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
 	device/lge/mako/WCNSS_cfg.dat:system/vendor/firmware/wlan/prima/WCNSS_cfg.dat \
 	device/lge/mako/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
 	device/lge/mako/WCNSS_qcom_wlan_nv.bin:system/etc/wifi/WCNSS_qcom_wlan_nv.bin
