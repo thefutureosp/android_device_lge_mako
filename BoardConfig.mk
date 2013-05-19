78f710f0b2e7a8ff12127ceee2279cb7f1eb6512
Francisco Franco <franciscofranco.1990@gmail.com>
2013-05-19 18:14:24 +0100

    Change Kernel defconfig file. I'm not sure this is exactly the place to change the defconfig, but this is where I found. Please correct it if this is wrong.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Try to build the kernel
-TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CONFIG := pa_mako_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_FLUENCE_INCALL := true
